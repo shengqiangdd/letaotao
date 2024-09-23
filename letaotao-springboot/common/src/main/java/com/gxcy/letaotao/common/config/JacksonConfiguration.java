1eda4ffe57ed80dc83108a9b51d06dc6c606a7b9
shengqiangdd <2361672268@qq.com>
2024-09-23 19:04:58 +0800

    init

--- letaotao-springboot/common/src/main/java/com/gxcy/letaotao/common/config/JacksonConfiguration.java
+++ letaotao-springboot/common/src/main/java/com/gxcy/letaotao/common/config/JacksonConfiguration.java
@@ -1,6 +1,8 @@
 package com.gxcy.letaotao.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,4 +20,12 @@ public class JacksonConfiguration {
             builder.serializerByType(Long.class, ToStringSerializer.instance);
         };
     }
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule()); // 支持时间格式
+        return objectMapper;
+    }
+
 }
